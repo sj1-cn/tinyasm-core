61e1068bd887478b9eaf16aac8f2bddb759f68b0
wangshilian <wangshilian@gmail.com>
2018-08-12 16:16:59 +0800

    refact

---
+++ nebula-asm/src/test/java/nebula/tinyasm/EnumBuilderSampleTest.java
@@ -0,0 +1,62 @@
+package nebula.tinyasm;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.Type;
+import org.objectweb.asm.util.ASMifier;
+import org.objectweb.asm.util.TraceClassVisitor;
+
+public class EnumBuilderSampleTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testStatusBuilder() throws Exception {
+		Type type = Type.getObjectType("com/nebula/dropwizard/core/Status");
+
+		byte[] code = EnumBuilder.dump(type, "STARTED", "FAILED", "COMPLETED");
+		byte[] codeExpected = EnumBuilderSampleDump.dump();
+
+		String strCode = toString(code);
+		String strCodeExpected = toString(codeExpected);
+		assertEquals("Code", strCodeExpected, strCode);
+	}
+
+	public static String toString(byte[] code) throws IOException {
+		ClassReader cr = new ClassReader(code);
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		ClassVisitor visitor = new TraceClassVisitor(null, new ASMifier(), pw);
+		cr.accept(visitor, ClassReader.EXPAND_FRAMES);
+		return sw.toString();
+	}
+
+	@Test
+	public void printClass() throws IOException {
+		System.out.println(toString(ClassBuilderMath.class.getName()));
+	}
+
+	public static String toString(String className) throws IOException {
+		ClassReader cr = new ClassReader(className);
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		ClassVisitor visitor = new TraceClassVisitor(null, new ASMifier(), pw);
+		cr.accept(visitor, ClassReader.EXPAND_FRAMES);
+		return sw.toString();
+	}
+}
