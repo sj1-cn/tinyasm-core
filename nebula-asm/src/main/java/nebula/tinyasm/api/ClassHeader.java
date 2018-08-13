f54ccbd014a073f8f1fca0346c471d9f733fff42
wangshilian <wangshilian@gmail.com>
2018-08-13 13:15:24 +0800

    classheader

---
+++ nebula-asm/src/main/java/nebula/tinyasm/api/ClassHeader.java
@@ -0,0 +1,31 @@
+package nebula.tinyasm.api;
+
+import static nebula.tinyasm.util.TypeUtils.typeOf;
+
+import java.util.function.Consumer;
+
+import org.objectweb.asm.Type;
+
+import nebula.tinyasm.ClassBuilder;
+
+public interface ClassHeader {
+
+	default ClassHeader annotation(Class<?> annotationClass) {
+		return vmAnnotation(typeOf(annotationClass), null);
+	}
+
+	default ClassHeader annotation(String annotationClass) {
+		return vmAnnotation(typeOf(annotationClass), null);
+	}
+
+	default ClassHeader annotation(Class<?> annotationClass, Object value) {
+		return vmAnnotation(typeOf(annotationClass), value);
+	}
+
+	ClassHeader vmAnnotation(Type annotationType, Object value);
+
+	ClassBody body();
+
+	ClassBuilder body(Consumer<ClassBody> mb);
+
+}
