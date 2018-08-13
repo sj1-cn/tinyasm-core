9994985ff1820846ce08a715ec1477babdeb7b33
wangshilian <wangshilian@gmail.com>
2018-08-14 00:50:42 +0800

    add test use class

---
+++ nebula-asm/src/test/java/nebula/tinyasm/TestBase.java
@@ -0,0 +1,36 @@
+package nebula.tinyasm;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.util.ASMifier;
+import org.objectweb.asm.util.TraceClassVisitor;
+
+public class TestBase {
+
+	public static String toString(byte[] code) throws IOException {
+		ClassReader cr = new ClassReader(code);
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		ClassVisitor visitor = new TraceClassVisitor(null, new ASMifier(), pw);
+		cr.accept(visitor, ClassReader.EXPAND_FRAMES);
+		return sw.toString().replaceAll("LineNumber\\([0-9]*\\,","LineNumber(1,");
+	}
+
+	public static String toString(String className) throws IOException {
+		ClassReader cr = new ClassReader(className);
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		ClassVisitor visitor = new TraceClassVisitor(null, new ASMifier(), pw);
+		cr.accept(visitor, ClassReader.EXPAND_FRAMES);
+		return sw.toString().replaceAll("LineNumber\\([0-9]*\\,","LineNumber(1,");
+	}
+
+	public TestBase() {
+		super();
+	}
+
+}
