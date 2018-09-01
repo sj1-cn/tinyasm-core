dc3484f6ff47666d7b5454f3db073add32055331
wangshilian <wangshilian@gmail.com>
2018-09-01 09:23:19 +0800

    simplize

--- src/main/java/nebula/tinyasm/data/ListMap.java
+++ src/main/java/nebula/tinyasm/data/ListMap.java
@@ -5,8 +5,12 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Stack;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
-public class ListMap<K, V> {
+public class ListMap<K, V> implements Iterable<V> {
 
 	Stack<V> stack = new Stack<>();
 	Map<K, V> maps = new HashMap<>();
@@ -63,6 +67,24 @@ public class ListMap<K, V> {
 
 	public List<V> list() {
 		return stack;
+	}
+
+	public ListMap<K, V> filter(Predicate<? super V> predicate) {
+		ListMap<K, V> newlist = new ListMap<>(namingHandler);
+		for (V v : this.stack) {
+			if (predicate.test(v)) {
+				newlist.push(v);
+			}
+		}
+		return newlist;
+	}
+
+	public <R> List<R> map(Function<? super V, ? extends R> mapper) {
+		return this.list().stream().map(mapper).collect(Collectors.toList());
+	}
+
+	public void foreach(Consumer<? super V> action) {
+		this.list().stream().forEach(action);
 	}
 
 	@Override
