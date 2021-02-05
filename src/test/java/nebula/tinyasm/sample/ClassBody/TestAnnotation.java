45848b50f90ca23036b67422ab7729f330a1aacd
wangshilian <wangshilian@sohu.com>
2021-02-05 18:03:46 +0800

    fix compile bugs

--- src/test/java/nebula/tinyasm/sample/ClassBody/TestAnnotation.java
+++ src/test/java/nebula/tinyasm/sample/ClassBody/TestAnnotation.java
@@ -2,8 +2,11 @@ package nebula.tinyasm.sample.ClassBody;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import static java.lang.annotation.ElementType.*;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target({TYPE,FIELD,METHOD,PARAMETER,LOCAL_VARIABLE})
 public @interface TestAnnotation {
 	String value() default "valueDefault";
 
